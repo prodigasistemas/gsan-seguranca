ae8ffc8ca8eaee1caff540d5edf2ce717a64fc58
Paulo Moura <paulociecomp@gmail.com>
2016-08-26 16:52:50 -0300

    add submenus às funcionalidades do menu

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -2,11 +2,26 @@ $(function(){
 
   $(".panel-modulo").click(function(event) {
     
-    // alert( $(this).data("modulo-id") )
     var moduloId = $(this).data("modulo-id");
 
     $(".row-submenu").hide();
     $(".row-submenu[data-modulo-id='" + moduloId + "']").show();
+
+  });
+
+  $(".funcionalidade-pai").click(function(event) {
+    
+    event.preventDefault();
+    var $submenu = $(this).next(".submenu");
+
+    if( $submenu.is(":visible") ){
+      $submenu.hide();
+      $(this).find("i").removeClass('fa-caret-down').addClass('fa-caret-right');
+    }
+    else{
+      $submenu.show();
+      $(this).find("i").removeClass('fa-caret-right').addClass('fa-caret-down');
+    }
 
   });
 
