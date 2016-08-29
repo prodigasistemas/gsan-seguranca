77bb669ed0d7935b22f92cbce9aad7d2b6acb84d
Paulo Moura <paulociecomp@gmail.com>
2016-08-29 16:41:47 -0300

    add icones para os modulos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  ICONES_MENU = {
+    atendimento_ao_publico: 'fa-users',
+    cadastro: 'fa-archive',
+    micromedicao: 'fa-battery-full',
+    faturamento: 'fa-bar-chart',
+    cobranca: 'fa-envelope-o',
+    arrecadacao: 'fa-balance-scale',
+    relatorios: 'fa-pie-chart',
+    gerencial: 'fa-line-chart',
+    seguranca: 'fa-lock',
+    batch: 'fa-cog',
+    financeiro: 'fa-dollar',
+    operacional: 'fa-cogs'
+  }
+
+  def get_icone(modulo)
+    nome_modulo = modulo.gsub(' ', '_').underscore.to_sym
+    ICONES_MENU[nome_modulo]
+  end
 end
