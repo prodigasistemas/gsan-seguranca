891bcbecefbe166c869e9b443db16404d8e01a63
Mateus Linhares <mateuslinhares@gmail.com>
2016-08-30 17:53:06 -0300

    utilizando cookie por dominio para fazer o SSO (wip: usar token ao inves de id)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :acesso_restrito
 
+  DOMAIN = ENV['GSAN_DOMAIN'] || 'localhost'
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -12,6 +14,8 @@ class ApplicationController < ActionController::Base
 
     limpar_sessao
     session[:usuario_id] = usuario.id
+
+    setar_cookie
   end
 
   def usuario_logado
@@ -27,11 +31,18 @@ class ApplicationController < ActionController::Base
       @usuario_logado = usuario_logado
       return @usuario_logado unless @usuario_logado.nil?
     end
-    session.clear
+
+    limpar_sessao
     redirect_to(new_session_path, alert: "Efetue seu login no GSAN")
   end
 
   def limpar_sessao
     session.clear
+    cookies.delete :gsan, domain: DOMAIN
+  end
+
+  def setar_cookie
+    cookies.delete :gsan, domain: DOMAIN
+    cookies.permanent[:gsan] = { value: usuario_logado.id, domain: DOMAIN }
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :acesso_restrito, only: [:new, :create]
+  protect_from_forgery except: :destroy
 
   layout 'home'
 
