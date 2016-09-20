dc84dbb129d1864ac96cce512c9cb9a6d23a2359
Paulo Moura <paulociecomp@gmail.com>
2016-09-20 18:07:01 -0300

    correcao no model usuario.

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,9 +1,10 @@
 class Usuario < ClienteAPI::Model
   extend ClienteAPI::Integracao::Autenticacao
 
-  attr_accessor :id, :nome, :nome_usuario, :senha, :empresa_id
+  attr_accessor :id, :nome, :nome_usuario, :senha, :empresa_id, :admin
 
   has_many :empresas
+  has_many :historico_arquivo_retornos
 
   def self.login(credenciais={})
     autenticar(credenciais)
