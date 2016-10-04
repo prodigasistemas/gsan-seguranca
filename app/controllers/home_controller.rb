89a1bd18c1cba69cf65992541ed9f7d0f97e9053
Mateus Linhares <mateuslinhares@gmail.com>
2016-10-04 17:36:47 -0300

    melhoria do código de geração do json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,35 +2,7 @@ class HomeController < ApplicationController
   layout 'application'
 
   def index
-    @menu_to_search = []
-
     menu = Menu.get(usuario_logado.id)
     @menu = MenuPresenter.new( menu )
-
-    to_json @menu[:menu][:submenu]
-  end
-
-  private
-
-  def to_json(submenus, categoria = "")
-    submenus.each do |submenu|
-      if submenu[:url].present?
-        @menu_to_search << funcionalidade(categoria, submenu[:descricao], submenu[:url])
-      else
-        if categoria.present?
-          to_json(submenu[:submenu], "#{categoria} - #{submenu[:descricao]}")
-        else
-          to_json(submenu[:submenu], submenu[:descricao])
-        end
-      end
-    end
-  end
-
-  def funcionalidade(categoria, descricao, url)
-    {
-      label: "#{categoria} - #{descricao}",
-      value: descricao,
-      url: url
-    }
   end
 end
